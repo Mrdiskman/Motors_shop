7f6151d8aba568b2cb2f50401dc19ef3fce1037d
kratzmendes <kratzmendes@gmail.com>
2023-02-16 15:23:11 -0300

    feat: get (all) announcements

--- backend/src/routes/announcement/announcement.routes.ts
+++ backend/src/routes/announcement/announcement.routes.ts
@@ -1,20 +1,33 @@
 import { Router } from "express";
 import deleteAnnouncementController from "../../controllers/announcements/deleteAnnouncement.controller";
+import getAnnouncementsController from "../../controllers/announcements/getAnnouncements.controller";
 import registerAnnouncementController from "../../controllers/announcements/registerAnnouncement.controllers";
 import { authUser } from "../../middlewares/users/authUser.middleware";
 
 const announcementsRoutes = Router();
 
 announcementsRoutes.post(
-    "/announcement", 
-    authUser, 
-    registerAnnouncementController
+  "/announcement", 
+  authUser, 
+  registerAnnouncementController
 ); //add "isEmployee" middleware
 
+announcementsRoutes.get(
+  "/announcements",
+  getAnnouncementsController
+);
+
+announcementsRoutes.get(
+  "/announcement/:id",
+  authUser, 
+//  getAnnouncementByIdController
+);
+
+
 announcementsRoutes.delete(
-    "/:id",
-    authUser, 
-    deleteAnnouncementController
+  "/announcement/:id",
+  authUser, 
+  deleteAnnouncementController
 ); //add "isEmployee" middleware
 
 export default announcementsRoutes;
