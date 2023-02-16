8e9bcf420d6e6d14d0bf371720552cca6c21bc44
kratzmendes <kratzmendes@gmail.com>
2023-02-16 15:06:16 -0300

    feat: delete announcement

--- backend/@types/express/index.d.ts
+++ backend/@types/express/index.d.ts
@@ -4,6 +4,7 @@ declare global{
     namespace Express{
         interface Request{
             idUser:string,
+            isAdm: boolean,
         }
     }
 }
--- backend/src/routes/announcement/announcement.routes.ts
+++ backend/src/routes/announcement/announcement.routes.ts
@@ -1,8 +1,20 @@
 import { Router } from "express";
+import deleteAnnouncementController from "../../controllers/announcements/deleteAnnouncement.controller";
 import registerAnnouncementController from "../../controllers/announcements/registerAnnouncement.controllers";
+import { authUser } from "../../middlewares/users/authUser.middleware";
 
-const routes = Router();
+const announcementsRoutes = Router();
 
-routes.post("/announcement", registerAnnouncementController);
+announcementsRoutes.post(
+    "/announcement", 
+    authUser, 
+    registerAnnouncementController
+); //add "isEmployee" middleware
 
-export default routes;
+announcementsRoutes.delete(
+    "/:id",
+    authUser, 
+    deleteAnnouncementController
+); //add "isEmployee" middleware
+
+export default announcementsRoutes;
