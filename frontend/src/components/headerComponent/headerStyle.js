6735e66d2159aadcc1c09d88a76f3ac4bc4edce5
Mrdiskman <pedrosandes2010@gmail.com>
2023-02-21 16:10:13 -0300

    headerComponent/Feat

--- frontend/src/components/headerComponent/headerStyle.js
+++ frontend/src/components/headerComponent/headerStyle.js
@@ -7,8 +7,6 @@ export const HeaderStyle = styled.header`
     justify-content: center;
     align-items: center;
 
-   
-       
     li{
         list-style: none;
         font-family: 'Inter', sans-serif;
@@ -29,6 +27,7 @@ export const HeaderStyle = styled.header`
         margin-left: 2%;
         gap: 5px;
         border-left: solid 2px #DEE2E6;
+        cursor: pointer;
     }
     .navBar{
         width: 70%;
@@ -76,9 +75,16 @@ export const HeaderStyle = styled.header`
         display: flex;
         justify-content: center;
         align-items: center;
-         
+
+        .menu{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
         .title{
             font-family: 'Inter', sans-serif;
+            margin-left: 30px;
             font-weight: 700;
             font-size: 30px;
             background-image: linear-gradient(90deg, rgba(2,0,36,1) 46%, rgba(69,41,230,1) 57%);
@@ -100,6 +106,7 @@ export const HeaderStyle = styled.header`
             background-color: transparent;
             border: none;
             font-size: 25px;
+            margin: 0 auto;
         }
         .navBar, .userDataContainer{
             display: none;
@@ -107,6 +114,7 @@ export const HeaderStyle = styled.header`
         
         .listContainerMobile{
             list-style: none;
+            padding: 0px;
         }
     
     }
@@ -117,6 +125,7 @@ export const HeaderStyle = styled.header`
         border-bottom: solid 1px black;
 
         .title{
+            margin-left: 10px;
             font-family: 'Inter', sans-serif;
             font-weight: 700;
             font-size: 30px;
@@ -134,5 +143,73 @@ export const HeaderStyle = styled.header`
         .listContainerMobile{
             list-style: none;
         }
+    }
+`
+
+export const NavBar = styled.nav`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .menu{
+        margin-top: 10px;
+        background-color: #FFFFFF;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 15px;
+    }
+    .option{
+        background-color: transparent;
+        border: none;
+        border-bottom: black 1px solid;
+        width: 35%;
+        padding-bottom: 5px;
+        color: #495057;
+        font-weight: 400;
+        font-size: 16px;
+        font-family: 'Inter', sans-serif;
+    }
+    .listMobile{
+        list-style: none;
+        font-family: 'Inter', sans-serif;
+        font-size: 16px;
+        font-weight: 400;
+        color: #495057;
+    }
+    .listContainerMobile{
+        display: flex;
+        gap: 20%;
+        list-style: none;
+        border-bottom: black 1px solid;
+        padding-bottom: 15px;
+    }
+`
+export const MenuOptions = styled.div`
+    z-index: 5;
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    align-items: flex-end;
+    .conteinerOptions{
+        width: 8%;
+        border: 1px solid black;
+        margin-right: 5%;
+        display: flex;
+        flex-direction: column;
+        
+        gap: 16px;
+    }
+    .option{
+        cursor: pointer;
+        color: #495057;
+        text-align: start;
+        background-color: transparent;
+        border: none;
+        font-family: 'Inter', sans-serif;
+        width: 100%;
+        font-weight: 400;
+        font-size: 16px;
+        padding-bottom: 5px;
     }
 `
