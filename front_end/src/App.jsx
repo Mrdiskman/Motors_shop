fe8f3c663ec830531c7b3d1174672a069040abe1
thiagonazarko <thnzk1@gmail.com>
2023-02-22 18:13:44 -0300

    vehicleCard components static

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,9 +1,13 @@
-import BackGround from './components/backgroundComponent/background';
-import Header from "./components/headerComponent/header"
+/* import BackGround from './components/backgroundComponent/background'; */
+import Header from "./components/headerComponent/header";
+import BuildVehicleCard from "./components/vehicleCardComponent/vehicleCard";
+import { GlobalStyle } from "./style/Global";
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <GlobalStyle />
+      <Header />
+      <BuildVehicleCard />
       {/* <BackGround/> */}
     </div>
   );
