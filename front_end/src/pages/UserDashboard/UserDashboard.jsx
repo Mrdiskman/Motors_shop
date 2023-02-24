e53ae6e357b96fc5d7678a2cad208ffaf997a45d
Mrdiskman <pedrosandes2010@gmail.com>
2023-02-24 09:36:57 -0300

    Seller profile Page/Feat

--- front_end/src/pages/UserDashboard/UserDashboard.jsx
+++ front_end/src/pages/UserDashboard/UserDashboard.jsx
@@ -7,53 +7,57 @@ import { carsData } from "../../components/vehicleCardComponent/carsDatabase";
 import { motorcyclesData } from "../../components/vehicleCardComponent/motorcyclesDatabase";
 import BuildVehicleCard from "../../components/vehicleCardComponent/vehicleCard";
 import { database } from "../../database";
+import { DivStylesAnnounce } from "./userDashBoardStyle";
 
 const UserDashboard = () => {
   return (
     <>
       <Header />
       <BackGroundStyle>
-        <BuildAnnouncerCard
-          name={database.user.name}
-          seller={database.user.seller}
-          descripition={database.user.descripition}
-        />
+        <DivStylesAnnounce>
+          <BuildAnnouncerCard
+            name={database.user.name}
+            seller={database.user.seller}
+            descripition={database.user.descripition}
+          />
+        </DivStylesAnnounce>
+        <p className="titleHome" id="cars">
+            Carros
+          </p>
+        <CarouselComponent>
+          {carsData.map((car, index) => (
+            <BuildVehicleCard
+              key={index}
+              img={car.img}
+              title={car.title}
+              descryption={car.descryption}
+              announcer={car.announcer}
+              km={car.km}
+              year={car.year}
+              price={car.price}
+            />
+          ))}
+        </CarouselComponent>
+
+        <p className="titleHome" id="motos">
+          Motos
+        </p>
+        <CarouselComponent>
+          {motorcyclesData.map((moto, index) => (
+            <BuildVehicleCard
+              key={index}
+              img={moto.img}
+              title={moto.title}
+              descryption={moto.descryption}
+              announcer={moto.announcer}
+              km={moto.km}
+              year={moto.year}
+              price={moto.price}
+            />
+          ))}
+        </CarouselComponent>
       </BackGroundStyle>
 
-      <h2 className="TitleHome" id="cars">
-        Carros
-      </h2>
-      <CarouselComponent>
-        {carsData.map((car, index) => (
-          <BuildVehicleCard
-            key={index}
-            img={car.img}
-            title={car.title}
-            descryption={car.descryption}
-            announcer={car.announcer}
-            km={car.km}
-            year={car.year}
-            price={car.price}
-          />
-        ))}
-      </CarouselComponent>
-      <h2 className="TitleHome" id="motos">
-        Motos
-      </h2>
-      <CarouselComponent>
-        {motorcyclesData.map((moto, index) => (
-          <BuildVehicleCard
-            key={index}
-            img={moto.img}
-            title={moto.title}
-            descryption={moto.descryption}
-            announcer={moto.announcer}
-            km={moto.km}
-            year={moto.year}
-            price={moto.price}
-          />
-        ))}
-      </CarouselComponent>
       <Footer />
     </>
   );
---
+++ front_end/src/pages/UserDashboard/userDashBoardStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const DivStylesAnnounce = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap");
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
