f42117f7efc5e8cc1c974bb05331abc6d16170f0
Mrdiskman <pedrosandes2010@gmail.com>
2023-02-24 00:33:45 -0300

    Fixed Mobile Announce

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,15 +1,17 @@
 
-/* import BackGround from './components/backgroundComponent/background'; */
-import AnnounceDetailPage from './pages/viewAnnounceDetailPage/announceDetailPage';
-import { GlobalStyle } from "./style/Global"; 
-import RoutesMain from "./routes";
-import Header from './components/headerComponent/header';
+import React from "react";
+import ContextsProvider from "./contexts/provider";
+import RoutesMain from "../src/routes/index";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-      <RoutesMain />
+      <ToastContainer />
+      <ContextsProvider>
+        <RoutesMain />
+      </ContextsProvider>
     </div>
   );
 }
