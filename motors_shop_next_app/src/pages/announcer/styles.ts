289814356aa189ff79eeaa4906240dbf98cb5a99
kratzmendes <kratzmendes@gmail.com>
2023-03-01 20:12:25 -0300

    feat/fix: announcer/home pages

---
+++ motors_shop_next_app/src/pages/announcer/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const HomeStyles = styled.nav`
+  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap");
+  width: 99vw;
+  height: 100%;
+  .container {
+    margin-left: 45px;
+    .auctions {
+      margin-top: 10%;
+    }
+  }
+`;
+
+export const TitleHome = styled.h2`
+
+  margin-bottom: 40px;
+  font-weight: 600;
+  font-size: 24px;
+  color: #0B0D0D;
+  margin-top: 13%;
+  margin-left: 3%;
+  font-family: "Lexend", sans-serif;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 30px;
+
+`
+
+export const DivStylesAnnounce = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap");
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
