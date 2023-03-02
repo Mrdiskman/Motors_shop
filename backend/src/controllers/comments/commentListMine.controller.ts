83f9b0015308829521704c906578db23582fdd2d
thiagonazarko <thnzk1@gmail.com>
2023-03-02 17:56:03 -0300

    fix: Routes (ainda falta resolver o problema dos deletes)

--- backend/src/controllers/comments/commentListMine.controller.ts
+++ backend/src/controllers/comments/commentListMine.controller.ts
@@ -3,9 +3,9 @@ import { instanceToPlain } from "class-transformer";
 import commentListMineService from "../../services/comments/commentListMine.service";
 
 const commentListMineController = async (req: Request, res: Response) => {
-  const comments = await commentListMineService();
+  /* const comments = await commentListMineService({userId: req.id});
 
-  return res.send(instanceToPlain(comments));
+  return res.send(instanceToPlain(comments)); */
 };
 
 export default commentListMineController;
