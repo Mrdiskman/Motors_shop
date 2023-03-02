ca1931a797ebee2a35151421aa622143000a3638
kalebesampaio <kalebesmp2002@gmail.com>
2023-03-02 15:05:03 -0300

    feat: add route list comment

--- backend/src/routes/comments/comment.routes.ts
+++ backend/src/routes/comments/comment.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import commentCreateController from "../../controllers/comments/commentCreate.controller";
+import commentListAllController from "../../controllers/comments/commentListAll.controller";
 import { authUser } from "../../middlewares/users/authUser.middleware";
 import cpfAlredyExistMiddleware from "../../middlewares/users/cpfAlredyExist.middleware";
 import emailAlredyExistMiddleware from "../../middlewares/users/emailAlredyExist.middleware";
 const commentRoutes = Router();
 
 commentRoutes.post("/comments", commentCreateController);
+commentRoutes.get("/comments", commentListAllController);
 
 export default commentRoutes;
