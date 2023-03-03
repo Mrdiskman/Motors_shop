959ad854ac97352d6234bfc1fabe05efd4c83167
thiagonazarko <thnzk1@gmail.com>
2023-03-03 00:24:56 -0300

    feat: modalComponent desktop

--- motors_shop_next_app/src/components/GeneralComponents/OptionsDesktop/index.tsx
+++ motors_shop_next_app/src/components/GeneralComponents/OptionsDesktop/index.tsx
@@ -4,21 +4,35 @@ import ModalEditProfile from "../ModalComponent/modalComponent";
 import { MenuOptionsStyled } from "./styled";
 
 const MenuOptions = () => {
-  const { handleOpenModal } = useContext(HeaderContext);
+  const { handleOpenModal, isModalOpen } = useContext(HeaderContext);
 
   return (
-    <MenuOptionsStyled>
-      <div className="conteinerOptions">
-        <button className="option" onClick={handleOpenModal}>
-          Editar Perfil
-        </button>
-        <ModalEditProfile />
-        {/* <ModalEditProfile /> */}
-        <button className="option">Editar endereço</button>
-        <button className="option">Meus Anúncios</button>
-        <button className="option + for onClick={logOut}">Sair</button>
-      </div>
-    </MenuOptionsStyled>
+    <>
+      {isModalOpen == true ? (
+        <MenuOptionsStyled>
+          <div className="conteinerOptions">
+            <button className="option" onClick={handleOpenModal}>
+              Editar Perfil
+            </button>
+            <ModalEditProfile />
+            <button className="option">Editar endereço</button>
+            <button className="option">Meus Anúncios</button>
+            <button className="option + for onClick={logOut}">Sair</button>
+          </div>
+        </MenuOptionsStyled>
+      ) : (
+        <MenuOptionsStyled>
+          <div className="conteinerOptions">
+            <button className="option" onClick={handleOpenModal}>
+              Editar Perfil
+            </button>
+            <button className="option">Editar endereço</button>
+            <button className="option">Meus Anúncios</button>
+            <button className="option + for onClick={logOut}">Sair</button>
+          </div>
+        </MenuOptionsStyled>
+      )}
+    </>
   );
 };
 
