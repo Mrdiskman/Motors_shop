0e5aef1df8252b0abdc6b31167934fe5476fb16b
Mrdiskman <pedrosandes2010@gmail.com>
2023-03-02 23:39:42 -0300

    Login compleate/feat

--- motors_shop_next_app/src/pages/login/index.tsx
+++ motors_shop_next_app/src/pages/login/index.tsx
@@ -1,24 +1,33 @@
 import Footer from "@/components/GeneralComponents/Footer";
 import Header from "@/components/GeneralComponents/Header";
+import { useForm } from "react-hook-form";
 import { LoginStyled } from "./styled";
 
 function Login() {
+  const {register, handleSubmit, formState:{errors}} = useForm()
+  function onSubmit(data:any){
+    const loginData = {
+        email:String(data.email),
+        password:String(data.password),
+    }
+   console.log(loginData)
+} 
   return (
     <>
       <Header />
-      <LoginStyled>
+      <LoginStyled onSubmit={handleSubmit(onSubmit)}>
         <form className="formLogin">
           <h2 className="typeForm">Login</h2>
           <label>Usuário</label>
-          <input type="text" placeholder="Digitar Usuário" />
+          <input type="text" placeholder="Digitar Usuário" {...register("email")}/>
           <label>Senha</label>
-          <input type="password" placeholder="Digitar Senha" />
-          <button className="forgetPassword">Esqueci minha senha</button>
+          <input type="password" placeholder="Digitar Senha" {...register("password")} />
+          <button className="forgetPassword" type="button">Esqueci minha senha</button>
           <button type="submit" className="send">
             Entrar
           </button>
           <p className="dontHaveAcc">Ainda não possui uma conta?</p>
-          <button className="register">Cadastrar</button>
+          <button className="register" type="button">Cadastrar</button>
         </form>
       </LoginStyled>
       <Footer />
