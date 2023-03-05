2c5c1f0c09383c96ce57ddf0d135ed4cf20e2d6c
thiagonazarko <thnzk1@gmail.com>
2023-03-05 17:44:09 -0300

    fix: deleteUserFeature mobile

--- backend/src/migrations/1678047752977-initialMigration.ts
+++ backend/src/migrations/1678047752977-initialMigration.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initialMigration1678041158870 implements MigrationInterface {
+export class initialMigration1678047752977 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
