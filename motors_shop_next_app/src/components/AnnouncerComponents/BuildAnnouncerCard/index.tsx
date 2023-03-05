362f990f21d949553a39c92e5174708f7307ed4a
Mrdiskman <pedrosandes2010@gmail.com>
2023-03-05 19:51:22 -0300

    Style Fix/Fix

--- motors_shop_next_app/src/components/AnnouncerComponents/BuildAnnouncerCard/index.tsx
+++ motors_shop_next_app/src/components/AnnouncerComponents/BuildAnnouncerCard/index.tsx
@@ -39,7 +39,7 @@ const BuildAnnouncerCard = ({ name, seller, descripition }: any) => {
     overlay: {
       backgroundColor: 'rgba(0, 0, 0, 0.15)'
     }
-  };
+  }; 
  
   return (
     <AnnouncerCard>
