f106848eb0373950ff9a7d7f8fb4d6147f4523a5
devmarques7 <dmarques4bussines@gmail.com>
2023-03-09 14:54:00 +0100

    feat : update project

--- motors_shop_next_app/src/components/GeneralComponents/CardAuction/index.tsx
+++ motors_shop_next_app/src/components/GeneralComponents/CardAuction/index.tsx
@@ -1,53 +1,50 @@
-import { CardAuctionStyle } from "./styled";
+import { Card, CardInfo } from "./styled";
 import { BiTimeFive } from "react-icons/bi";
 import { BsArrowRight } from "react-icons/bs";
 
 const CardAuction = ({ owner }: any) => {
   return (
-    <CardAuctionStyle>
-      <section className="sectionMainCard">
-        <div className="divTime">
-          <BiTimeFive color="#4529E6" />
-          <p>01:58:00</p>
-        </div>
-        <h3 className="titleCard">
-          Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200
-        </h3>
-        <span className="descriptionCard">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem...
+    <Card className="card">
+      <CardInfo
+        className="card info"
+        img="https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"
+      >
+        <span className="card info time">
+          <BiTimeFive className="icon clock" />
+          <span className="clock">01:58:00</span>
         </span>
-        <section className="sectionUser">
-          <img
-            src="https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"
-            alt=""
-          />
-          <p>Rodrigo Tavares</p>
-        </section>
-        <section className="containerInfo">
-          <section className="info">
-            <div className="divInfo">
-              <p>2013</p>
-            </div>
-            <div className="divInfo">
-              <p>0 KM</p>
-            </div>
-          </section>
-          <p>R$ 00.000,00</p>
-        </section>
-      </section>
-      {owner ? (
+        <div className="card desctiption">
+          <h3 className="card desctiption title">
+            Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200
+          </h3>
+          <p className="card desctiption sub">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem...
+          </p>
+        </div>
+        <div className="card owner">
+          <span className="card owner inicials">T A</span>
+          <span className="card owner name">Tiago Almeida</span>
+        </div>
+        <div className="card info rotation">
+          <div className="info">
+            <span className="info box km">0 KM</span>
+            <span className="info box year">2019</span>
+          </div>
+          <span className="info price">R$ 00.000,00</span>
+        </div>
+      </CardInfo>
+      <button className="card link">
+        <span className="text">Acessar página do leilão</span>
+        <BsArrowRight className="link" />
+      </button>
+      {owner && (
         <section className="containerPageButton">
           <button className="button_edit">Editar</button>
           <button className="button_edit">Ver como</button>
         </section>
-      ) : (
-        <section className="containerPage">
-          <h3>Acessar página do leilão</h3>
-          <BsArrowRight className="setaHover" color="#ffffffff" />
-        </section>
       )}
-    </CardAuctionStyle>
+    </Card>
   );
 };
 
