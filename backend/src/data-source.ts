b17541c6a550ef55c1893f886bef5ec23864d43d
thiagonazarko <thnzk1@gmail.com>
2023-03-10 09:26:19 -0300

    feature emptyDB

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -4,9 +4,6 @@ import { User } from "./entities/users";
 import { Announcement } from "./entities/announcement";
 import { Address } from "./entities/address";
 import { Comment } from "./entities/comments/comment.entity";
-import { initialMigration1678375517535 } from "./migrations/1678375517535-initialMigration";
-import { createMigrations1678375535283 } from "./migrations/1678375535283-createMigrations";
-
 
 export const AppDataSource =
   process.env.NODE_ENV === "test"
@@ -24,5 +21,5 @@ export const AppDataSource =
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
         entities: [User, Address, Announcement, Comment],
-        migrations: [initialMigration1678375517535, createMigrations1678375535283],
+        migrations: [],
       });
